69ff36909432f2e706d76b8da934935344fe179f
BuildTools <unconfigured@null.spigotmc.org>
2021-05-28 22:24:16 +0800

    fix ssrf-04-redis bug

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/chall/NISRA_Challs
-tmux new -s chall -d docker-compose up
-docker run --name chall_index -v $(pwd)/index.html:/usr/share/nginx/html/index.html:ro -p 80:80 -d --rm nginx
